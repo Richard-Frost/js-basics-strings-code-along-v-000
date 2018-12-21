d1c2010122df8658da28a095717a43794382395b
Richard Frost <frost.rchrd@gmail.com>
2018-12-21 04:09:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,3 @@ const currentUser = 'Autumn';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;

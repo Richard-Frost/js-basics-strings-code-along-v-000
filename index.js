5040c7017c48c576f4fd4788439d15c060d3ee5c
Richard Frost <frost.rchrd@gmail.com>
2018-12-21 04:10:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Autumn';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
